9a6fd69ecfb55f84b91009c37b19183eee98c74f
OlenaPatlata <elenapat468@gmail.com>
2022-08-24 09:32:25 +0300

    created server

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+require("dotenv").config();
+const mongoose = require('mongoose');
+const { PORT = 3000 } = process.env;
+const { CONNECT } = process.env;
+
+
+mongoose.connect(CONNECT)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1)
+  });
