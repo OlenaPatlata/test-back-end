37996b937be69e9a1e4bce68839980ca2c72214d
OlenaPatlata <elenapat468@gmail.com>
2022-08-26 09:43:56 +0300

    add heroku

--- controllers/users/reVerifyEmail.js
+++ controllers/users/reVerifyEmail.js
@@ -16,7 +16,7 @@ const reVerifyEmail = async (req, res) => {
   const mail = {
     to: email,
     subject: 'Подтверждение email',
-    html: `<p>Thanks for signing up with App! You must follow this link of registration to activate your account:</p></br><a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Verify email</a>`
+    html: `<p>Thanks for signing up with App! You must follow this link of registration to activate your account:</p></br><a target="_blank" href="https://test-back-end-1.herokuapp.com/api/users/verify/${verificationToken}">Verify email</a>`
   };
   await sendMail(mail);
   res.status(200).json("Verification email sent");
