af4357b06ae38788a865ff5c825f8e964881fb91
OlenaPatlata <elenapat468@gmail.com>
2022-08-26 13:34:45 +0300

    3

--- controllers/users/findOneByEmail.js
+++ controllers/users/findOneByEmail.js
@@ -3,8 +3,8 @@ const { createError } = require('../../helpers');
 
 const findOneByEmail = async (req, res) => {
   const { email } = req.body;
-  const result = await User.find({ email }, "-createdAt -updatedAt -password -token -verify -verificationToken -age");
-  if (result?.length===0) {
+  const result = await User.findOne({ email }, "-createdAt -updatedAt -password -token -verify -verificationToken -age");
+  if (!result) {
     throw createError(404, `Contact with email: ${email} didn't find`)
   };
   res.status(200).json(result);
